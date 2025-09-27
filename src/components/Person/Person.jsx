03c3d30390288d424bbc8c7f59fdff34fb1cb05d
DDefty <dawid.kogut99@gmail.com>
2025-09-27 09:33:48 +0200

    Refactor age check in Person component for clarity

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,10 +1,11 @@
 export function Person({ person }) {
   const { name, age = null, sex, isMarried, partnerName } = person;
+  const hasAge = age != null;
 
   return (
     <section className="Person">
       <h2 className="Person__name">My name is {name}</h2>
-      {age !== null && <p className="Person__age">I am {age}</p>}
+      {hasAge && <p className="Person__age">I am {age}</p>}
       {isMarried ? (
         <p className="Person__partner">
           {partnerName} is my {sex === 'm' ? 'wife' : 'husband'}
